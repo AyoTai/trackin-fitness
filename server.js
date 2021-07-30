e56b645ddda16fec95c605079805bb3570a1870c
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-30 15:32:14 -0700

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 // Database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",  
-{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
 
 // Get Routes
 app.get("/", (req, res) =>{
